e20d344e8e5d25064f9afa1915c17e4eeebac294
Donovan Daniels <dwddaniels262@gmail.com>
2021-02-12 15:31:09 -0600

    reeeee

--- src/Other/Types.d.ts
+++ src/Other/Types.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types */
 export type FilterFlags<Base, Condition> = {
 	[Key in keyof Base]: Base[Key] extends Condition ? Key : never;
 };
@@ -5,14 +6,14 @@ export type InvertedFilterFlags<Base, Condition> = {
 	[Key in keyof Base]: Base[Key] extends Condition ? never : Key;
 };
 export type FilterReturnType<Base, Condition> = {
-	[Key in keyof Base]: Base[Key] extends AnyFunction ? ReturnType<Base[Key]> extends Condition ? Key : never : never;
+	[Key in keyof Base]: Base[Key] extends (...args: Array<any>) => any ? ReturnType<Base[Key]> extends Condition ? Key : never : never;
 };
 export type InvertedFilterReturnType<Base, Condition> = {
-	[Key in keyof Base]: Base[Key] extends AnyFunction ? ReturnType<Base[Key]> extends Condition ? never : Key : never;
+	[Key in keyof Base]: Base[Key] extends (...args: Array<any>) => any ? ReturnType<Base[Key]> extends Condition ? never : Key : never;
 };
 export type AllowedNames<Base, Condition> = FilterFlags<Base, Condition>[keyof Base];
 export type BetterFilter<Base, Condition> = Pick<Base, keyof Omit<Base, AllowedNames<Base, Condition>>>;
-export type WithoutFunctions<T> = BetterFilter<T, AnyFunction>;
+export type WithoutFunctions<T> = BetterFilter<T, Function>;
 export type Without<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 export type WithOptional<T, O extends keyof T> = Omit<T, O> & DeepPartial<Pick<T, O>>;
 export type SomeOptional<T, R extends keyof T = never> = WithOptional<T, keyof Without<T, R>>;
@@ -20,7 +21,7 @@ export type DeepPartial<T> = {
 	[P in keyof T]?: Partial<T[P]>;
 };
 export type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
-export type ThenReturnType<T extends AnyFunction> = ThenArg<ReturnType<T>>;
+export type ThenReturnType<T extends (...args: any) => any> = ThenArg<ReturnType<T>>;
 export type OmitFirstArg<F> = F extends (x: unknown, ...args: infer P) => infer R ? (...args: P) => R : never;
 export type valueof<T> = T[keyof T];
 export type ArrayOrObject<T = unknown> = Array<T> | { [K in keyof T]: T[K]; };
