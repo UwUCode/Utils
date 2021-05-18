a829d4244f491d0a737085ea231df69c6fa83871
Donovan_DMC <hewwo@yiff.rocks>
2021-05-17 23:41:07 -0500

    third time's the charm

--- src/Other/pid.ts
+++ src/Other/pid.ts
@@ -4,17 +4,13 @@ export default function pid(path: string) {
 	const b = dirname(path);
 	if (!fs.existsSync(b)) fs.mkdirpSync(b);
 	fs.writeFileSync(path, process.pid.toString());
-	function remove(...args: Array<number | NodeJS.Signals>) {
+	function remove() {
 		try {
 			fs.unlinkSync(path);
 		} catch (e) {
 			// not handling this error
 		}
-		process.kill(process.pid, args[0]);
 	}
 
-	process
-		.once("exit", remove.bind(null))
-		.once("SIGINT", remove.bind(null))
-		.once("SIGTERM", remove.bind(null));
+	process.once("exit", remove.bind(null));
 }
