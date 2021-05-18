e5a562d5f3f9b16708c3fe0342314c41bae60e77
Donovan_DMC <hewwo@yiff.rocks>
2021-05-17 19:39:11 -0500

    [pid] rekill process with previous signal

--- src/Other/pid.ts
+++ src/Other/pid.ts
@@ -4,17 +4,17 @@ export default function pid(path: string) {
 	const b = dirname(path);
 	if (!fs.existsSync(b)) fs.mkdirpSync(b);
 	fs.writeFileSync(path, process.pid.toString());
-	function handle() {
+	function remove(code?: number, signal?: NodeJS.Signals) {
 		try {
 			fs.unlinkSync(path);
 		} catch (e) {
 			// not handling this error
 		}
-		process.exit(0);
+		process.kill(process.pid, code ?? signal);
 	}
 
 	process
-		.on("exit", handle)
-		.on("SIGINT", handle)
-		.on("SIGTERM", handle);
+		.on("exit", remove.bind(null))
+		.on("SIGINT", remove.bind(null))
+		.on("SIGTERM", remove.bind(null));
 }
