ccc58f693cac6ea7c8ab41d9e2b1209388403e2b
Donovan_DMC <hewwo@yiff.rocks>
2021-05-18 01:14:44 -0500

    [pid] listen to all the common signals

--- src/Other/pid.ts
+++ src/Other/pid.ts
@@ -4,15 +4,20 @@ export default function pid(path: string) {
 	const b = dirname(path);
 	if (!fs.existsSync(b)) fs.mkdirpSync(b);
 	fs.writeFileSync(path, process.pid.toString());
-	function remove() {
+	function remove(type: string | null) {
 		try {
 			fs.unlinkSync(path);
 		} catch (e) {
 			// not handling this error
 		}
+
+		process.kill(process.pid, type ?? undefined);
 	}
 
 	process
-		.once("exit", remove.bind(null))
-		.once("SIGINT", remove.bind(null));
+		.once("SIGINT", remove.bind(null, "SIGINT"))
+		.once("SIGHUP", remove.bind(null, "SIGHUP"))
+		.once("SIGQUIT", remove.bind(null, "SIGQUIT"))
+		.once("SIGTERM", remove.bind(null, "SIGTERM"))
+		.once("exit", remove.bind(null, null));
 }
