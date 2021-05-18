273bdd67dcddf24426580af59f2ca6f547904b07
Donovan_DMC <hewwo@yiff.rocks>
2021-05-17 19:48:47 -0500

    [pid] only catch signals once

--- src/Other/pid.ts
+++ src/Other/pid.ts
@@ -4,17 +4,17 @@ export default function pid(path: string) {
 	const b = dirname(path);
 	if (!fs.existsSync(b)) fs.mkdirpSync(b);
 	fs.writeFileSync(path, process.pid.toString());
-	function remove(code?: number, signal?: NodeJS.Signals) {
+	function remove(...args: Array<number | NodeJS.Signals>) {
 		try {
 			fs.unlinkSync(path);
 		} catch (e) {
 			// not handling this error
 		}
-		process.kill(process.pid, code ?? signal);
+		process.kill(process.pid, args[0]);
 	}
 
 	process
-		.on("exit", remove.bind(null))
-		.on("SIGINT", remove.bind(null))
-		.on("SIGTERM", remove.bind(null));
+		.once("exit", remove.bind(null))
+		.once("SIGINT", remove.bind(null))
+		.once("SIGTERM", remove.bind(null));
 }
