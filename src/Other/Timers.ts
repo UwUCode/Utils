cdc3791c54390796cd0744157918321fbfda24e9
Donovan Daniels <hewwo@yiff.rocks>
2021-08-01 11:13:21 -0500

    changes to Timers

--- src/Other/Timers.ts
+++ src/Other/Timers.ts
@@ -1,57 +1,47 @@
+import Time from "../Functions/Time";
 import Logger from "logger";
-import { performance } from "perf_hooks";
 import crypto from "crypto";
 
 interface Timer {
-	start: number | null;
-	end: number | null;
+	start: bigint | null;
+	end: bigint | null;
 }
 
 export default class Timers {
 	id: string;
-	timers: {
+	private timers: {
 		[k: string]: Timer;
 	};
-	log: boolean;
-	constructor(log?: boolean, id?: string) {
+	private log: (label: string, info: string) => void;
+	constructor(log?: ((label: string, info: string) => void) | boolean, id?: string) {
 		this.id = id || crypto.randomBytes(10).toString("hex");
 		this.timers = {};
-		this.log = !!log;
+		this.log = log === true ? (label: string, info: string) => Logger.debug(label, info) : log === false ? () => undefined : log as Timers["log"];
 	}
 
 	start(label: string) {
 		if (Object.keys(this.timers).includes(label)) throw new TypeError(`Timer with the label "${label}" has already been started.`);
 		// if (this.log) Logger.info(`Timers[${this.id}]`, `Timer with label ${label} started.`);
 		const t = this.timers[label] = {
-			start: parseFloat(performance.now().toFixed(3)),
+			start: process.hrtime.bigint(),
 			end: null
 		};
 		return t.start;
 	}
 
-	end(label: string) {
+	end(label: string, dec?: number) {
 		if (!Object.keys(this.timers).includes(label)) throw new TypeError(`[${this.id}] Timer with the label "${label}" has not been started.`);
 		if (this.timers[label].end !== null) throw new TypeError(`[${this.id}] Timer with the label "${label}" has already ended.`);
 
-		this.timers[label].end = parseFloat(performance.now().toFixed(3));
-		if (this.log) {
-			// Logger.info(`Timers[${this.id}]`, `Timer with label ${label} ended.`);
-			Logger.debug(`Timers[${this.id}]`, `${label} took ${this.calc(label, label)}ms`);
-		}
-		return this.timers[label].end;
+		this.timers[label].end = process.hrtime.bigint();
+		this.log(`Timers[${this.id}]`, `${label} took ${this.calc(label, dec)}ms`);
+		return this.timers[label].end!;
 	}
 
-	calc(start: string, end?: string) {
-		if (!end) end = start;
-		return typeof start === "number" && typeof end === "number" ?
-			parseFloat(
-				(end - start).toFixed(3)
-			) : parseFloat(
-				(
-					(this.timers && this.timers[end] ? this.timers[end].end : 0)!
-					-
-					(this.timers && this.timers[start] ? this.timers[start].start : 0)!
-				).toFixed(3)
-			);
+	calc(label: string, dec: number | undefined, raw: true): number;
+	calc(label: string, dec?: number, raw?: false): string;
+	calc(label: string, dec?: number, raw = false) {
+		const v = Number((this.timers[label]?.end ?? 0n) - (this.timers[label]?.start ?? 0n));
+		return raw === true ? parseFloat((v).toFixed(dec)) : Time.convert(parseFloat((v).toFixed(dec)), "ns", dec);
 	}
 }
