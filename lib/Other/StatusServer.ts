53080a33bddbda3bb84de7034be9c4c5568ff2a5
Donovan Daniels <hewwo@yiff.rocks>
2023-03-04 13:20:12 -0600

    should have done it this way

--- lib/Other/StatusServer.ts
+++ lib/Other/StatusServer.ts
@@ -1,13 +1,10 @@
-import { type Server, createServer } from "node:http";
+import { createServer } from "node:http";
 
 export default function statusServer(check: () => boolean, host = "127.0.0.1", port = 3621) {
-    const server = createServer((req, res) => {
+    return createServer((req, res) => {
         res.writeHead(check() ? 204 : 503, {
             "Content-Type":   "text/plain",
             "Content-Length": 0
         }).end();
     }).listen(port, host);
-    const closeFunction: (() => void) & { server: Server; } = () => void server.close();
-    closeFunction.server = server;
-    return closeFunction;
 }
