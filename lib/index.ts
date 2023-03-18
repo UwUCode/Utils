89afef9389f626c91709af02a78ca54074bbc24c
Donovan Daniels <hewwo@yiff.rocks>
2023-03-18 16:09:40 -0500

    move status server to its own package

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-export * from "./types.js";
+export type * from "./types.js";
 
 export { default as Internal } from "./Functions/Internal.js";
 export { default as Strings } from "./Functions/Strings.js";
@@ -8,5 +8,4 @@ export { default as Timer } from "./Other/Timer.js";
 export { default as ReNice } from "./Other/ReNice.js";
 export { default as EnvOverride } from "./Other/EnvOverride.js";
 export { default as BuildDate } from "./Other/BuildDate.js";
-export { default as StatusServer } from "./Other/StatusServer.js";
 export { default as pid } from "./Other/pid.js";
