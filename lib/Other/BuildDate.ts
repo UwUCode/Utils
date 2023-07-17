997067deb20c2fbd513065b4ef5327e59a497834
Donovan Daniels <hewwo@yiff.rocks>
2023-07-17 11:00:00 -0500

    make build date epoch

--- lib/Other/BuildDate.ts
+++ lib/Other/BuildDate.ts
@@ -1,11 +1,16 @@
+import Time from "../Functions/Time.js";
 import { access, readFile, writeFile } from "node:fs/promises";
 
 export default async function buildDate(dir: string) {
-    const pkg = await readFile(`${dir}/package.json`, "utf8").then(JSON.parse) as { buildDate: string | null; };
-    console.log("Current Build Date:", pkg.buildDate === null ? "Unknown" : `${String(pkg.buildDate).slice(0, 2)}/${String(pkg.buildDate).slice(2, 4)}/${String(pkg.buildDate).slice(4, 8)}`);
-    const d = new Date();
-    (pkg.buildDate) = `${d.getMonth().toString().padStart(2, "0")}${(d.getDate() + 1).toString().padStart(2, "0")}${d.getFullYear()}`;
-    console.log("New Build Date:", pkg.buildDate === null ? "Unknown" : `${String(pkg.buildDate).slice(0, 2)}/${String(pkg.buildDate).slice(2, 4)}/${String(pkg.buildDate).slice(4, 8)}`);
+    const d = Date.now();
+    const pkg = await readFile(`${dir}/package.json`, "utf8").then(JSON.parse) as { buildDate: number | null; };
+    if (pkg.buildDate !== undefined && pkg.buildDate !== null && typeof pkg.buildDate !== "number") {
+        console.log("Current build date is invalid, discarding.");
+        pkg.buildDate = null;
+    }
+    console.log("Current Build Date:", pkg.buildDate === null ? "Unknown" : Time.formatDateWithPadding({ date: pkg.buildDate }));
+    pkg.buildDate = d;
+    console.log("New Build Date:", pkg.buildDate === null ? "Unknown" : Time.formatDateWithPadding({ date: d }));
     await writeFile(`${dir}/package.json`, JSON.stringify(pkg, undefined, "  "));
     // if built, write to build dir as well
     if (await access(`${dir}/dist/package.json`).then(() => true, () => false)) {
